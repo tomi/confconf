3ee40bb88b20bb12e35b1b516b3e7b13e91fd251
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2021-11-10 10:48:58 +0200

    fix(confconf): export missing types
    Export ConfigProvider and ValidationError types so they can be used when needed.

--- packages/confconf/src/index.ts
+++ packages/confconf/src/index.ts
@@ -2,3 +2,7 @@ export * from "./confconf";
 export * from "./staticConfig";
 export * from "./envConfig";
 export * from "./devOnlyConfig";
+
+import type { ConfigProvider } from "./configProvider";
+import type { ValidationError } from "./ValidationError";
+export type { ConfigProvider, ValidationError };
