3e0eb4338640776e4f3316a61f23418a40165a19
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2021-12-29 20:02:38 +0200

    improve error handling capabilities

---
+++ .changeset/tasty-fans-prove.md
@@ -0,0 +1,7 @@
+---
+"@confconf/confconf": patch
+---
+
+add isValidationError type guard function
+
+This allows checking if throw error is of a type ValidationError
--- packages/confconf/src/ValidationError.ts
+++ packages/confconf/src/ValidationError.ts
@@ -1,7 +1,12 @@
 import type { ErrorObject } from "ajv";
 
+export type { ErrorObject };
+
 export class ValidationError extends Error {
   constructor(public readonly validationErrors: ErrorObject[]) {
     super(`Configuration validation failed: ${validationErrors.map((e) => e.message).join(", ")}`);
   }
 }
+
+export const isValidationError = (error: any): error is ValidationError =>
+  error instanceof ValidationError;
